5bf02241158ab29d78e129df0ef89a8de54bc745
NemanjaT1992 <nemanja.tomasevic71@gmail.com>
2020-04-21 21:04:10 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+const githubRepoEndpoint = "https://api.github.com/users/t0mashawk/repos";
+const fetchOptions = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+type GithubRepo = {
+  name: string;
+  html_url: string;
+};
+
+const fetchRepos = async (): Promise<any> => {
+  try {
+    const result = await fetch(githubRepoEndpoint, fetchOptions);
+    return await result.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 function App() {
+  const [repos, setRepos] = useState<GithubRepo[]>([]);
+  const [serchTerm, setSerchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchRepos().then((repos) => {
+      setRepos(repos);
+      setLoading(false);
+    });
+  }, []);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSerchTerm(event.currentTarget.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={serchTerm} onChange={handleInputChange}></input>
+      {loading ? (
+        <p>Loading repos...</p>
+      ) : (
+        <ul>
+          {repos
+            .filter((repo: GithubRepo) =>
+              repo.name.toLowerCase().includes(serchTerm.toLowerCase())
+            )
+            .map((repo: GithubRepo, index) => (
+              <li key={index}>
+                <a href={repo.html_url}>{repo.name}</a>
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 }
